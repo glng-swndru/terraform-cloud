f87aed7edebda449aa4c2eac3ddfa721748794ec
Gilang <gilangswandaru27@gmail.com>
2025-03-23 18:07:33 +0800

    edit resource name

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "web_service" {
 }
 
 resource "aws_lb" "web_service" {
-  name               = "${var.cluster_name}-${var.environment}-load-balancer"
+  name               = "${var.cluster_name}-${var.environment}-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_service.id]
@@ -63,7 +63,7 @@ resource "aws_lb" "web_service" {
 }
 
 resource "aws_lb_target_group" "web_service" {
-  name     = "${var.cluster_name}-${var.environment}-target-group"
+  name     = "${var.cluster_name}-${var.environment}-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
