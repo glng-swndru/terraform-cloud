6093a766fdfe280daed2f77cd8d9122c955404a2
Gilang <gilangswandaru27@gmail.com>
2025-03-23 18:38:01 +0800

    update

--- staging/ec2/main.tf
+++ staging/ec2/main.tf
@@ -1,6 +1,6 @@
 module "web-server-prod" {
   source = "../../modules/ec2"
-   
+
   ami                    = "ami-0520f976ad2e6300c"
   instance_type          = "t2.micro"
   tag_name               = "web-server"
@@ -10,7 +10,7 @@ module "web-server-prod" {
   key_pair               = "terraform-key-pair"
   asg_availability_zones = ["us-west-2a", "us-west-2b"]
   elb_availability_zones = ["us-west-2a", "us-west-2b"]
-  vpc_id                 = "vpc-00feacb6d220c5212"
-  subnets                = [ "subnet-0bb70691292e64ea7", "subnet-07ce57e703be85c3b"  ]
-  environment = "staging"
+  vpc_id                 = aws_vpc.main.id # VPC ID
+  subnets                = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id] # Subnet IDs
+  environment            = "staging"
 }
