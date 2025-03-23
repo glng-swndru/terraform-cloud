7c69cb1aa85a0677da75181a7899e6ffc179a5b1
Gilang <gilangswandaru27@gmail.com>
2025-03-23 19:44:33 +0800

    update

--- production/ec2/backend.tf
+++ production/ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket         = "practice-remote-states"
+        bucket         = "practice-remote-state-123"
         key            = "production/ec2"
         region         = "us-west-2"
         encrypt        = true
